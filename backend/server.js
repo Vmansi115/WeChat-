de2ef411e8ad3d88262802b24d12122e7bd3aa1c
Mansi Verma <99127354+Vmansi115@users.noreply.github.com>
2024-09-23 21:21:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -13,7 +12,6 @@ import {app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
 dotenv.config();
 
 app.use(express.json());
@@ -22,12 +20,6 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
-
-app.use(express.static(path.join(__dirname,"/frontend/dist")));
-
-app.get("*", (req,res) => {
-    res.sendFile(path,join(__dirname,"frontend","dist","index.html"));
-});
 
 server.listen(5000 ,() => {
     connectToMongoDB();
